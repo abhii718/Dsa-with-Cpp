0d30be7ec81d5d73630e8fb047aaa5e0c334dfe5
abhii718 <abhinaysingh47639@gamil.com>
2023-10-22 17:16:33 +0530

    new code

---
+++ tut43.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+protected:
+    int roll_no;
+
+public:
+    void set_number(int a)
+    {
+        roll_no = a;
+    }
+    void print_number(void)
+    {
+        cout << "your roll number is " << roll_no << endl;
+    }
+};
+
+class test : virtual public student
+{
+protected:
+    float maths, physics;
+
+public:
+    void set_marks(float m1, float m2)
+    {
+        maths = m1;
+        physics = m2;
+    }
+    void print_marks(void)
+    {
+        cout << "your result is here :"<< endl;
+        cout << "maths :" << maths << endl;
+        cout << "physics :" << physics << endl;
+    }
+};
+class sports : virtual public student
+{
+protected:
+    float score;
+
+public:
+    void set_score(float sc)
+    {
+        score = sc;
+    }
+    void print_score(void)
+    {
+        cout << "you PT score is " << score << endl;
+    }
+};
+class result : public test, public sports
+{
+private:
+    float total;
+
+public:
+    void display(void)
+    {
+        total = maths + physics + score;
+        print_number();
+        print_marks();
+        print_score();
+        cout << "your total score is" << total << endl;
+    }
+};
+main()
+{
+    result r;
+    r.set_number(34);
+    r.set_marks(78.5, 65.5);
+    r.set_score(4);
+    r.display();
+    return 0;
+}
