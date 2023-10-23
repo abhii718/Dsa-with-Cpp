ff530e12a9894602fe7a2ed364585debc33c1811
abhii718 <abhinaysingh47639@gamil.com>
2023-10-23 08:57:10 +0530

    new

---
+++ 24_stack_using_arry.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    // struct stack s;
+    // s.size = 80;
+    // s.top = -1;
+    // int *arr = (int *)malloc(s.size * sizeof(int));
+
+
+    struct stack *s;
+    s->size = 80;
+    s->top = -1;
+    int *arr = (int *)malloc(s->size * sizeof(int));
+
+
+    // pushing an element manually
+    s->arr[0] = 7;
+    s->top++;
+
+    
+    // check if stack is empty
+    if (isEmpty(s))
+    {
+        printf("stack is empty");
+    }
+    else
+    {
+        printf(" stack is not empty");
+    }
+    return 0;
+}
