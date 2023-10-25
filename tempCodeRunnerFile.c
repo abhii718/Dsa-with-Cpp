e6e038b11596b3c13a134da5f618d906a5c0a6e0
abhii718 <abhinaysingh47639@gamil.com>
2023-10-25 10:59:38 +0530

    n

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,10 @@
- q->next;
+struct Node *insertAfternode(struct Node *head, struct Node *previousnode, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    ptr->next = previousnode->next;
+    previousnode->next = ptr;
+
+    return head;
+};
