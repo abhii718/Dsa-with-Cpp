cf206eead7af3ac3576aba1be8e9265f306e8c9c
abhii718 <abhinaysingh47639@gamil.com>
2023-10-25 11:20:32 +0530

    new

---
+++ stacklinkedpushpop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct Node
+{
+}
+{
+    int data;
+    struct Node *next; /* data */
+};
+
+int main()
+{
+    struct Node *top = NULL;
+    push(top, 78);
+    return 0;
+}
